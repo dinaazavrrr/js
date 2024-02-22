b9e36fa4d8883787f03dccb19283afecd5ff25bc
dinaazavrrr <dianalutsyuk4347@gmail.com>
2024-02-22 18:58:39 +0200

    Added index.html file

---
+++ main.js
@@ -0,0 +1,27 @@
+// let age = 18;
+// let name = "Diana Lutsyuk";
+
+// console.log(age);
+// console.log(name);
+
+// alert("Мене звати " + "Diana Lutsyuk, " + "мені " + "18 " + "років.");
+
+// const userName = prompt("Як вас зввти?");
+// const userAge = prompt("Скільки вам років?");
+// console.log(userName);
+// console.log(userAge);
+
+// console.log(`Мене звати ${userName}, мені ${userAge} років.`);
+
+let number1 = +prompt("Введіть перше число:");
+let number2 = +prompt("Введіть друге число:");
+
+let sum = number1 +  number2
+let sub = number1 - number2
+let mul = number1 * number2
+let div = number1/number2
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
