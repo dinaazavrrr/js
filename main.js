a6a9a2190c05864468fdacbbf6abb70293420aa1
dinaazavrrr <dianalursyuk4347@gmail.com>
2024-02-22 20:59:46 +0200

    Added main.js file

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-// let age = 18;
-// let name = "Diana Lutsyuk";
+let age = 18;
+let name = "Diana";
+name = "Diana Lutsyuk";
 
-// console.log(age);
-// console.log(name);
+console.log(age);
+console.log(name);
 
-// alert("Мене звати " + "Diana Lutsyuk, " + "мені " + "18 " + "років.");
+ alert("Мене звати " + name + " мені " + age + " років.");
 
 // const userName = prompt("Як вас зввти?");
 // const userAge = prompt("Скільки вам років?");
@@ -13,15 +14,15 @@
 
 // console.log(`Мене звати ${userName}, мені ${userAge} років.`);
 
-let number1 = +prompt("Введіть перше число:");
-let number2 = +prompt("Введіть друге число:");
+// let number1 = +prompt("Введіть перше число:");
+// let number2 = +prompt("Введіть друге число:");
 
-let sum = number1 +  number2
-let sub = number1 - number2
-let mul = number1 * number2
-let div = number1/number2
+// let sum = number1 +  number2
+// let sub = number1 - number2
+// let mul = number1 * number2
+// let div = number1/number2
 
-console.log(sum);
-console.log(sub);
-console.log(mul);
-console.log(div);
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
