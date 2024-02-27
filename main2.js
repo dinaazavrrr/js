4aba2da6676cbabc016ac5d06a76beb18c95d84d
dinaazavrrr <dianalursyuk4347@gmail.com>
2024-02-27 03:11:13 +0200

    Added main2.js file

---
+++ main2.js
@@ -0,0 +1,48 @@
+//задача 1
+// let number1 = +prompt("Введіть перше число:");
+// let number2 = +prompt("Введіть друге число:");
+
+// let rem = number1 % number2
+// let pow = number2 ** number1
+
+// console.log(rem);
+// console.log(pow);
+
+//задача 2
+// let num = 7;
+// num +=5;
+// num -=2;
+// num *=2;
+
+// console.log(++num +2);
+
+
+
+//Задача 3
+// let salary = 8;
+// salary %= 2;
+// console.log(salary);
+// let num = 3;
+// num %= 2;
+// console.log(num);
+// let number = 5;
+// number %= 3;
+// console.log(number);
+
+//Задача 4
+// let value = +prompt("Назвіть число більше або рівне 18");
+// let yes = value >= 18;
+// console.log( yes);
+
+//Задача 5
+let x = +prompt("Enter x:"); 
+let y = +prompt("Enter y:");
+
+[x, y] = [y, x];
+
+console.log(x);
+console.log(y);
+
+
+
+
